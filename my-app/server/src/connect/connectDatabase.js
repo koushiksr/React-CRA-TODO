00b33f464e72176834f3ebcd21d43f1d6b8e5cb2
koushik s r <51875120+koushiksr@users.noreply.github.com>
2024-01-07 15:57:32 +0530

    Add files via upload

--- my-app/server/src/connect/connectDatabase.js
+++ my-app/server/src/connect/connectDatabase.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 const env = require("../config/envConfig");
 const connectDatabase = async () => {
   try {
-    const mongoURI = env.MONGODB_URI || "";
+    const mongoURI =
+      env.MONGODB_URI ||
+      "mongodb+srv://koushik:koushik@employeedatabase.uqho6xi.mongodb.net/authdetails?retryWrites=true&w=majority";
     if (!mongoURI) {
       throw new Error("MONGODB_URI environment variable not set");
     }
